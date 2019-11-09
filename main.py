d5a8064c551f1ea54801616a74ff6ce4ac3bf699
satu-n <shunkichi.sato@gmail.com>
2019-11-10 07:27:20 +0900

    refactored

--- main.py
+++ main.py
@@ -2,55 +2,30 @@ import gather as ga
 import concat as co
 import json
 import sys
-import os
 
 
-def main(config):
+def main(config, wtb, out):
+
+    with open(config) as f:
+        cfg = json.load(f)
+
+    scr = cfg['gather']['scr']  # 1回のスクロール量
+    slp = cfg['gather']['slp']  # 次のスクロールまでの休止時間（秒）
+    rat = cfg['concat']['rat']  # スクロール量に対する、クリップ領域の縦幅の比率
+    top = cfg['concat']['top']
+    lef = cfg['concat']['lef']
+    rig = cfg['concat']['rig']
+
+    ga.main(config, scr, slp, wtb, 0)
+    co.main(config, rat, top, lef, rig, out)
+
+
+if __name__ == '__main__':
     print("Hello!")
+
     # python main.py whiteboard output
     wtb = sys.argv[1]  # キャプチャ対象のホワイトボードID
     out = sys.argv[2]  # 出力ファイル名
 
-    with open(config) as f:
-        c = json.load(f)
-    scr = c['gather']['scr']  # 1回のスクロール量
-    slp = c['gather']['slp']  # 次のスクロールまでの休止時間（秒）
-    rat = c['concat']['rat']  # スクロール量に対する、クリップ領域の縦幅の比率
-    top = c['concat']['top']
-    bot = int(top + scr * rat)
-    lef = c['concat']['lef']
-    rig = c['concat']['rig']
-
-    drv = ga.webdriver.Chrome()
-
-    try:
-        ga.initialize(drv, wtb)
-    except KeyboardInterrupt:
-        print("Ctrl+C pressed.")
-        print("Now terminating driver...")
-        drv.quit()
-        print("Bye!")
-        sys.exit()
-    try:
-        ga.main(drv, scr, slp, 0)
-    except KeyboardInterrupt:
-        print("Ctrl+C pressed.")
-        print("{} screenshots saved.".format("some"))
-        print("Now terminating driver...")
-    finally:
-        drv.quit()
-
-    image_name_pattern = os.path.join(c['temporary_shots_dir'], c['temporary_shot_prefix'] + '*.png')
-    images = co.clip(image_name_pattern, top, bot, lef, rig)
-
-    out_file = os.path.join(c['out_dir'], out)
-    co.save(out_file,
-            co.cv2.vconcat(images),
-            c['temporary_img_for_pdf'])
-    print("Successfully {} images are clipped and concatenated to: ".format(len(images)))
-    print(out_file)
+    main('config.json', wtb, out)
     print("See you!")
-
-
-if __name__ == '__main__':
-    main('config.json')
